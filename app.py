3e9879d3c459284725c691bb8c4abaa4820e4e7d
Christian Dallago <code@dallago.us>
2017-10-13 11:23:19 +0200

    Added CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, abort
 from src.predadr import Predadr
+from flask_cors import CORS
 
 predictor = Predadr()
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
